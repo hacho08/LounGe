9399c026386e902b4072edbc64feb753d1b907dd
hacho08 <whgusdk0819@gmail.com>
2024-09-09 15:18:11 +0900

    finished project setting

---
+++ lounge/src/test/java/com/irijeoriyorijori/lounge/LoungeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.irijeoriyorijori.lounge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoungeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
