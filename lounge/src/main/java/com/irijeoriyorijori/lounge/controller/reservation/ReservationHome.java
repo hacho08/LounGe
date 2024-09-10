721988cfb2d6e58c6bce4f1d12e933270343a12b
nalin <nalin.lina.kim@gmail.com>
2024-09-10 14:54:15 +0900

    reservation frontend in process

--- lounge/src/main/java/com/irijeoriyorijori/lounge/controller/reservation/ReservationHome.java
+++ lounge/src/main/java/com/irijeoriyorijori/lounge/controller/reservation/ReservationHome.java
@@ -6,8 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ReservationHome {
 
-
-//    @GetMapping("reservation")
-//    public String createReservation(){}
+    @GetMapping("/reservation")
+    public String bookingPage() {
+        return "reservation/booking"; // 예약 페이지
+    }
 
 }
