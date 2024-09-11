0597ea26b1c65a21530864af64c2e4a4c130aac7
hacho08 <whgusdk0819@gmail.com>
2024-09-11 09:40:47 +0900

    fixed modal

--- lounge/src/main/java/com/irijeoriyorijori/lounge/controller/reservation/ReservationHomeController.java
+++ lounge/src/main/java/com/irijeoriyorijori/lounge/controller/reservation/ReservationHomeController.java
@@ -1,7 +1,9 @@
 package com.irijeoriyorijori.lounge.controller.reservation;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class ReservationHomeController {
@@ -9,5 +11,18 @@ public class ReservationHomeController {
     @GetMapping("/reservation")
     public String bookingPage() {
         return "reservation/booking"; // 예약 페이지
+    }
+
+    @GetMapping("/reservation/complete")
+    public String reservationComplete(Model model) {
+        // 예약 완료 화면으로 이동
+        return "reservation/reservationComplete";
+    }
+
+    @GetMapping("/reservation/{productType}")
+    public String reservationDetail(@PathVariable(name="productType") String productType,
+                                    Model model) {
+        model.addAttribute("productType", productType);
+        return "reservation/booking";
     }
 }
