bae6b6b14a6e56c1a0a41778ebe87ef1333829d9
hacho08 <whgusdk0819@gmail.com>
2024-09-11 17:23:50 +0900

    created reviewList, modified header

---
+++ lounge/src/main/resources/static/js/reviewlist.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const categoryButtons = document.querySelectorAll('.category');
+
+    // 카테고리 필터 클릭 시
+    categoryButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            // 모든 버튼의 active 클래스 제거
+            categoryButtons.forEach(btn => btn.classList.remove('active'));
+            // 클릭한 버튼에 active 클래스 추가
+            button.classList.add('active');
+        });
+    });
+});
