4bab1c03d913b595e53078105777dc5dda122783
22yeonni <lisa3472@naver.com>
2024-09-11 09:13:54 +0900

    clander revised

--- lounge/src/main/java/com/irijeoriyorijori/lounge/controller/reservation/ReservationHomeController.java
+++ lounge/src/main/java/com/irijeoriyorijori/lounge/controller/reservation/ReservationHomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ReservationHomeController {
-    
+
     @GetMapping("/reservation")
     public String bookingPage() {
         return "reservation/booking"; // 예약 페이지
